8f1df653ffdc69b01a7e8487a9f3e79128cb13e2
lp64ace <akomikron@gmail.com>
2025-02-18 21:33:24 +0200

    nigger

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -6,7 +6,7 @@ export const signup = async (name, email, password) => {
         console.log(response.data);
         return response.data;
     } catch (error) {
-        console.error("Error during signup:", error.response.data.error);
+        console.error("Error during signup:", error);
         throw error;
     }
 };
